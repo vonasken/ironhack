17862e1cc06148ca7af299026a4a19900aad0e67
Marc Von Asken <vonasken@gmail.com>
2016-11-05 03:26:07 -0400

    Ironhack FOlder Update

---
+++ week3/Day14/Vikings/app.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var vikings = require("./lib/vikings.js");
+
+console.log("")
+
+var viking1 = new Vikings( "Mayork", 50, 10)
+
+var viking2 = new Vikings( "Yazork", 40, 10)
+
+
+
---
+++ week3/Day14/Vikings/lib/vikings.js
@@ -0,0 +1,15 @@
+class Vikings {
+
+	constructor (name, health, strength ){
+		this.name = name
+		this.health = health
+		this.strength = strength
+
+	}
+
+	pit () { 
+
+	}
+}
+
+module.exports = Vikings;
