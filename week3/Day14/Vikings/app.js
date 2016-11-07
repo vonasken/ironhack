30f0eb77e0d920dc1b8ea834a2530afd0a9cd04e
Marc Von Asken <vonasken@gmail.com>
2016-11-06 23:29:17 -0500

    Day14 Viking update

--- week3/Day14/Vikings/app.js
+++ week3/Day14/Vikings/app.js
@@ -1,12 +1,19 @@
 "use strict";
 
-var vikings = require("./lib/vikings.js");
+var Vikings = require("./lib/vikings.js");
+
+var Pit = require("./lib/pit.js");
+
+var fs = require("fs")
 
 console.log("")
+
+
 
 var viking1 = new Vikings( "Mayork", 50, 10)
 
 var viking2 = new Vikings( "Yazork", 40, 10)
 
+var fighting = new Pit(viking1, viking2)
 
-
+fighting.fight();
--- week3/Day14/Vikings/lib/pit.js
+++ week3/Day14/Vikings/lib/pit.js
@@ -1,4 +1,4 @@
-class pit {
+class Pit {
 
 	constructor(viking1, viking2){
 
@@ -6,6 +6,41 @@ class pit {
 		this.viking2 = viking2
 	}
 
+		fight() {
+
+			setTimeout(after3Seconds, 3000);
+			
+
+
+			console.log("It all started with...")
+
+			function after3Seconds () {
+
+			console.log("2 Vikings enter the arena, only one will survive... ");
+
+				}
+
+				setTimeout(after7Seconds, 7000);
+
+					function after7Seconds (){
+			
+					console.log("or be beaten to an inch of his life. ")
+
+				};
+
+			function start () {
+
+				var vikingHealth = this.viking1.health 
+				viking2.health = 50
+
+				while viking
+			} 
+
+
+		}
+
 
 
 }
+
+module.exports = Pit;
