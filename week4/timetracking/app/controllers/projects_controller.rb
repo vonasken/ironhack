39ecb5916a94067f65d52c936ab1a8a5b2c1753e
Marc Von Asken <vonasken@gmail.com>
2016-11-09 11:18:42 -0500

    tmtrck

--- week4/timetracking/app/controllers/projects_controller.rb
+++ week4/timetracking/app/controllers/projects_controller.rb
@@ -7,5 +7,26 @@ class ProjectsController < ApplicationController
 		render 'index'
 
 	end
+	def show
+
+			@my_project = Project.find(params[:id])
+			render 'show'		
+
+	end
+
+	def new 
+
+		@my_project = Project.new
+		render "new"
+	end
+
+	def create 
+		@my_project = Project.new(
+			:name => params[:project][:name],
+			:description => params[:project][:description])
+		@my_project.save
+
+		redirect_to "/projects/#{@my_project.id}"
+	end
 
 end
--- week4/timetracking/config/routes.rb
+++ week4/timetracking/config/routes.rb
@@ -15,5 +15,11 @@ post '/calculate', to: 'calculator#process_addition'
 
 get '/projects', to: 'projects#index'
 
+get '/projects/new', to: 'projects#new'
+
+get "/projects/:id", to: "projects#show", as: :project
+
+post "/projects", to: "projects#create"
+
 end
 
