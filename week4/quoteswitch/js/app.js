c310d83a51753ab127a8dd3c8318b19f0e7e14cf
Marc Von Asken <vonasken@gmail.com>
2016-11-14 22:01:17 -0500

    Quote Switch

---
+++ week4/quoteswitch/js/app.js
@@ -0,0 +1,13 @@
+var quotes = [
+"Do. Or do not. There is no try. - Yoda ",
+"The critical ingredient is getting off your butt and doing something. It’s as simple as that. A lot of people have ideas, but there are few who decide to do something about them now. Not tomorrow. Not next week. But today. The true entrepreneur is a doer, not a dreamer. –Nolan Bushnell",
+"“To any entrepreneur: if you want to do it, do it now. If you don’t, you’re going to regret it.” - Catherine Cook",
+"“It’s not about ideas. It’s about making ideas happen.” - Scott Belsky",
+"“There’s nothing wrong with staying small. You can do big things with a small team.” - Jason Fried",
+"“I’ve missed more than 9,000 shots in my career. I’ve lost almost 300 games. 26 times I’ve been trusted to take the game's winning shot and missed. I’ve failed over and over and over again in my life and that's why I succeed.” - Michael Jordan",
+"'The best time to plant a tree was 20 years ago. The second best time is now.' - Chinese proverb.",
+"“If you just work on stuff that you like and you’re passionate about, you don’t have to have a master plan with how things will play out.” - Mark Zuckerberg",
+"“The secret to successful hiring is this: look for the people who want to change the world.” - Marc Benioff"
+
+]
+
