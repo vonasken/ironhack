1e6e46014e9a17862cd83da0bf1f8f03567b6e77
Marc Von Asken <vonasken@gmail.com>
2016-11-20 19:38:04 -0500

    folder update

--- week4/spotify-skeleton-master/js/app.js
+++ week4/spotify-skeleton-master/js/app.js
@@ -23,8 +23,9 @@ function artistSearch () {
  var searchQuery = $(".js-search-term").val();
      $(".js-search-results").empty();
 $.ajax({
-      url: "https://api.spotify.com/v1/search?type=artist&query=" + searchQuery,
       type: 'GET',
+      url: "GET https://api.spotify.com/v1/albums/" + searchQuery + "/tracks" + ,
+
       success: showResults,
       error: handleError,
   });
